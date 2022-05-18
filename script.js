04d4de5e07eeba240a8501bf742e47c159693682
Elson Otake <elsonotake@yahoo.com.br>
2022-05-18 09:22:09 -0300

    Update menu mobile js

--- script.js
+++ script.js
@@ -4,25 +4,23 @@
 
 const body = document.querySelector('body');
 const header = document.querySelector('.header_b');
+const hamburger = document.querySelector('.mob_menu');
 
 // Classes for close menu
 
-const classNames = ['menu_link', 'mob_menu', 'mob_close'];
+const classNames = ['menu_link', 'mob_close'];
+
+hamburger.addEventListener('click', (event) => {
+  header.classList.add('active');
+  body.classList.add('noscroll');
+});
 
 header.addEventListener('click', (event) => {
-  console.log(event.target);
   const clickedElementClassName = event.target.classList[0];
   const shouldToggle = classNames.some((className) => className === clickedElementClassName);
 
-  if (shouldToggle) {
-    header.classList.toggle('active');
-
-    // Vertical scroll disabled
-
-    if (header.classList.contains('active')) {
-      body.classList.add('noscroll');
-    } else {
-      body.classList.remove('noscroll');
-    }
+  if (shouldToggle && header.classList.contains('active')) {
+    header.classList.remove('active');
+    body.classList.remove('noscroll');
   }
 });
